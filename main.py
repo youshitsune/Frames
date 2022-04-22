792ebac2e5b5f264a6a88672e068b7a8d1022837
Luka Jovanovic <jovanovic.luka3214@gmail.com>
2022-04-22 15:26:18 +0200

    Adding GUI

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile
+from tkinter import StringVar
 import cv2
 import os
-import click
 
-@click.command()
-@click.argument('path', type=str)
+root = Tk()
+
+root.title("Frames")
+
+root.geometry("400x400")
+
+text = StringVar()
+
+def open_file():
+    file = askopenfile(mode='r',filetypes=[("All Files", '*.*')])
+    if file:
+        file_path = os.path.abspath(file.name)
+        start(file_path)
+
+caution = Label(root, text = "Caution: When you select file you can't stop making images from GUI. If you want to stop making images you have to do it from terminal").pack()
+file_button = Button(root, text="Select a video file", command=open_file).pack(pady=20)
+label = Label(root, textvariable=text).pack()
+exit_button = Button(root, text="Exit", width=20, command=root.destroy).pack(pady=20)
 def start(path):
+    global label
     video = cv2.VideoCapture(path)
 
     try:
@@ -21,17 +41,15 @@ def start(path):
 
         if ret:
             name = './data/frame' + str(currentframe) + '.jpg'
-            print('Creating...' + name)
-
+            text.set(f"Creating... {name}")
+            root.update_idletasks()
             cv2.imwrite(name, frame)
 
             currentframe +=1
-
         else:
             break
 
     cam.release()
     sys.exit()
 
-if __name__ == '__main__':
-    start()
+root.mainloop()
